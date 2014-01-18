dfeaf2b79918dd600fbda85129df5aca0feb23d0
Émile Filteau-Tessier <emile.filteau@gmail.com>
2014-01-18 09:53:13 -0500

    calendar crap
    Signed-off-by: Émile Filteau-Tessier <emile.filteau@gmail.com>

--- server/action/dao/CalendarDAO.php
+++ server/action/dao/CalendarDAO.php
@@ -5,17 +5,17 @@ class CalendarDAO {
 	
 	public static function syncCalendar($user_id, $events) {
 		$connection = Connection::getConnection();
-		
+		$priority = 0;
 		foreach($events as $event) {
 		
 			$statement = $connection->prepare('INSERT INTO calendar_events 
 			(user_id, START_DATE, END_DATE, name, description, priority) VALUES (:user_id, :START_DATE, :END_DATE, :name, :description, :priority)'); 
 			$statement->bindParam(":user_id", $user_id);
-			$statement->bindParam(":START_DATE", $events["start_date"]);
-			$statement->bindParam(":END_DATE", $events["end_date"]);
-			$statement->bindParam(":name", $events["name"]);
-			$statement->bindParam(":description", $events["description"]);
-			$statement->bindParam(":priority", 0);
+			$statement->bindParam(":START_DATE", $event["start_date"]);
+			$statement->bindParam(":END_DATE", $event["end_date"]);
+			$statement->bindParam(":name", $event["name"]);
+			$statement->bindParam(":description", $event["description"]);
+			$statement->bindParam(":priority", $priority);
 			
 			$statement->setFetchMode(PDO::FETCH_ASSOC);
 			$statement->execute();
