2f2f99219633a2918ee3648badeb8e8aaccb798a
Adam Smih <adamsmith.mtl@gmail.com>
2014-01-18 04:52:47 -0500

    Started the tab view for the tasks

--- client/src/com/cloudteammanager/TeamActivity.java
+++ client/src/com/cloudteammanager/TeamActivity.java
@@ -1,15 +1,37 @@
 package com.cloudteammanager;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class TeamActivity extends Activity {
-
+	private TabHost tabHost;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_team);
+
+		tabHost = (TabHost) findViewById(R.id.tab_host);
+		tabHost.setup();
+
+		TabSpec spec1 = tabHost.newTabSpec("TAB 1");
+		spec1.setContent(R.id.tab1);
+		spec1.setIndicator("TAB 1");
+
+		TabSpec spec2 = tabHost.newTabSpec("TAB 2");
+		spec2.setIndicator("TAB 2");
+		spec2.setContent(R.id.tab2);
+
+		TabSpec spec3 = tabHost.newTabSpec("TAB 3");
+		spec3.setContent(R.id.tab3);
+		spec3.setIndicator("TAB 3");
+		tabHost.addTab(spec1);
+		tabHost.addTab(spec2);
+		tabHost.addTab(spec3);
 	}
 
 	@Override
